94dccb3d6a39c97d667853097232389e76330801
PHUICMT <PHUICMT@github.com>
2022-09-16 09:38:34 +0700

    ✅ update

--- server.py
+++ server.py
@@ -22,35 +22,30 @@ Payload.max_decode_packets = 500
 DOMAIN = 'database'
 PORT = 27017
 
-emotion_result_default = {
-   "1": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "2": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "3": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "4": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "5": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "6": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "7": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "8": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "9": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-}
-
 emotion_result = {
-   "1": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "2": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "3": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "4": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "5": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "6": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "7": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "8": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-   "9": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
-}
+        "1": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
+        "2": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
+        "3": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
+        "4": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
+        "5": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
+        "6": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
+        "7": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
+        "8": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
+        "9": {"fear": 0, "happy": 0, "sad": 0, "neutral": 0},
+    }
 
 print("[INFO] Server is started...")
 
 directory_operation = DirectoryTask()
 file_operation = FileTask()
 general_operation = GeneralTask()
+
+def reset_emotion_result():
+    for key in emotion_result:
+        emotion_result[key]["fear"] = 0
+        emotion_result[key]["happy"] = 0
+        emotion_result[key]["sad"] = 0
+        emotion_result[key]["neutral"] = 0
 
 @socketio.on('user_connected')
 def client_connect(user_data):
@@ -72,16 +67,20 @@ def end_section(user_data):
     video_path = saved_video_path + "/" + user_data['user_id'] + ".mp4"
 
     save_result = file_operation.save_video(saved_image_path, video_path)
+    print("[INFO] Video saved: ", save_result)
     if save_result:
-        print("[INFO] Video is saved...")
         emotion_result_percentage = general_operation.get_emotion_result_percentage(emotion_result)
-        emotion_result = emotion_result_default
         emit('emotion', emotion_result_percentage)
+        
+@socketio.on('reset_data')
+def reset_data():
+    print("[INFO] Client reset data...")
+    reset_emotion_result()
+    
         
 @socketio.on('disconnect')
 def disconnect():
     print("[INFO] Client disconnected...")
-    
 
 @socketio.on('image')
 def image(data_image): 
@@ -142,7 +141,7 @@ def save_result():
     except errors.ServerSelectionTimeoutError as err:
         print("[ERROR] MongoDB connection failed...")
         return "MongoDB connection failed"
-        
+    
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=5000)
